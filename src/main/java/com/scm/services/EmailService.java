3937ae055c59bdf80d7c8675c52dbad7dbc929ca
Shivam Sharma <141565887+02SHIVAM3@users.noreply.github.com>
2025-05-27 12:41:39 +0530

    Update EmailService.java

--- src/main/java/com/scm/services/EmailService.java
+++ src/main/java/com/scm/services/EmailService.java
@@ -4,15 +4,15 @@ import java.io.InputStream;
 
 public interface EmailService {
 
-    //
+    // send email API
     void sendEmail(String to, String subject, String body);
 
     void sendEmail(String[] to, String subject, String body);
 
-    //
+    // With HTML API 
     void sendEmailWithHtml(String to , String subject, String htmlContent);
 
-    //
+    // With attachment
     void sendEmailWithAttachment(String to, String subject, String body, File file);
 
     void sendEmailWithAttachment(String to, String subject, String body, InputStream is);
